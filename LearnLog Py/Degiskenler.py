8727b00fe8f48b76f097ca48f6a6c74e1d7e9302
Hilal Kepir <kepirhilal@gmail.com>
2026-03-01 20:49:13 +0300

    Create Degiskenler.py

---
+++ LearnLog Py/Degiskenler.py
@@ -0,0 +1,17 @@
+#Degiskenler
+
+Kisi = ["Python",1999,"Istanbul","Blogger"]
+
+d1 = "Hello World"
+d2 = 89
+d3 = 2.54
+
+
+# Degisken Isimlendirirken Onemli
+"""
+Ozel semboller kullanilmaz
+Python'a ait ifadeler degisken adi olarak kullanilmaz
+Python da gercek sabit kavrami yoktur
+Degisken isimleri buyuk kucuk harfe duyarlidir
+
+"""
