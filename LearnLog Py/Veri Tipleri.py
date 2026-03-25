f4678b594caf6f6d419e02e7ec0fd8284dec8574
Hilal Kepir <kepirhilal@gmail.com>
2026-03-25 18:41:53 +0300

    update

--- LearnLog Py/Veri Tipleri.py
+++ LearnLog Py/Veri Tipleri.py
@@ -11,6 +11,8 @@ a = ''
 a = """ """
 print(type(a))
 
+print("----\n\n")
+
 #Liste
 liste = [2,3,5,5,4,7,8,9,41]
 print("Ilk liste:", liste)
@@ -61,6 +63,8 @@ print(max(liste2))
 print(3 in liste2)
 print(3 in liste)
 
+print("----\n\n")
+
 #Demet(Tuple)
 #Nasil olusturulursa oyle kalir
 demet = tuple()
@@ -71,11 +75,126 @@ print(demet)
 demet =("***")
 print(demet)
 
-demet = ("hi","admin","coding")
+demet = ("hi","hi","admin","coding")
 
 print(demet)
 print(demet[0])
+print(demet[-1])
 print(demet.count("hi"))
 print(demet.index("coding"))
 
+print("----\n\n")
+
+
 #Küme (Set)
+# Bir eleman yalnizca bir kere bulunur. Tekrar edemez.
+# Indexleme durumu yoktur. Karmasik sirada olabilir.
+# Eleman eklenip cikarilabilir
+kume = set()
+kume = {'a','b','c'}
+# kume = {} dictionary
+print(type(kume))
+
+#Set Methods
+set1 = {3,4,5,1}
+set2 = {1,9,2,3}
+
+set1.add(11)
+print("set1 : ",set1)
+set1.difference(set2)
+set1.discard(11)
+print(set1)
+set1.intersection(set2) #kesisim
+set1 = set1.union(set2)
+set1 = set1.union({5,5,5})
+
+print("----\n\n")
+
+# Sozluk(Dictionary)
+# Her veri bir key ile tutulur ve o anahtarin bir degeri (value) vardir.
+# key mutlaka int veya str olacak
+# value herhangi bir veri tipinde olabilir
+# Liste ile ayni tuple ile farkli olarak eleman eklenip cikarilabilir.
+
+bos_sozluk = {}
+print(type(bos_sozluk)) 
+print(bos_sozluk) # {}
+
+sozluk = dict()
+print(sozluk) # {}
+
+eng_dict = {
+    "1" : "one",
+    "2" : "two",
+    "3" : "three"
+}
+
+print(eng_dict)
+print(eng_dict.items())
+print(eng_dict["1"])
+eng_dict["4"] = "four" 
+del eng_dict["1"]
+print(eng_dict.values())
+# eng_dict.pop("anahtar")
+eng_dict.pop("2")
+
+print("----\n\n")
+
+#Stringden Diger Veri Tiplerine Gecis
+a = "111"
+
+a = int(a)
+print(type(a))
+a = str(a)
+
+a = float(a)
+print(type(a))
+a = str(a)
+
+a = complex(a)
+print(type(a))
+a = str(a)
+
+a = bool(a)
+print(type(a))
+a = str(a)
+
+a = list(a)
+print(type(a))
+a = str(a)
+
+a = tuple(a)
+print(type(a))
+a = str(a)
+
+a = set(a)
+print(type(a))
+a = str(a)
+
+"""
+a = dict(a)
+print(type(a))
+a = str(a)
+
+string dict gecisi yapilamaz 
+"""
+
+#Int'den diger veri tiplerine gecis
+# str, float, bool gecis var
+#tuple,list,set,dict gecis yok
+
+#boolean 
+#str, int, float, complex gecis var
+
+#list
+#str,tuple,set cevrilebilir
+
+#Stringden Listeye
+metin = "l like learning new programs."
+liste = metin.split(" ")
+print(liste)
+
+#Listeden Stringe
+eklenecek_listem = ["Top","Secret",":"]
+yeni_metin = "**".join(eklenecek_listem)
+print(yeni_metin)
