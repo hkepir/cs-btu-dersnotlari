9e062ac2fd417c82a8e8267a7f431e2014cd53e9
Hilal Kepir <kepirhilal@gmail.com>
2026-04-06 18:35:45 +0300

    updated

--- LearnLog Py/8-Class & Object.py
+++ LearnLog Py/8-Class & Object.py
@@ -71,3 +71,96 @@ print(book_default.printInfo())
 book1 = book("Siyah Lale","Alexsander Dumas")
 print(book1.printInfo())
 
+print("--------")
+
+#Class Variable & Instance Variable
+class Student():
+    #class variable(uretilen her nesne icin ortak)
+    school_name = "Python Academy"
+    student_number = 0
+    ID = 1000
+
+    def __init__(self, name, age):
+        # Instance variables (her nesneye ozgu)
+        self.name = name
+        self.age = age
+        Student.ID +=10 
+        self.ID = Student.ID
+        Student.student_number += 1
+
+    def I_method(self):
+        print("I method")
+    
+student1 = Student("Hilal", 22)
+student2 = Student("Ayse", 24)
+
+# accessing class variable 
+print("Okul adi:", Student.school_name)
+
+# accessing instance variable 
+print("std 1:", student1.name, "-", student1.age)
+print("std 2:", student2.name, "-", student2.age)
+
+
+print("std1 okul:", student1.school_name)
+print("std2 okul:", student2.school_name)
+
+
+Student.school_name = "Advanced Python School"
+print("new school name:", student1.school_name)
+print("new school name:", student2.school_name)
+
+student1.school_name = "Updated"
+print("new school name:", student1.school_name)
+print("new school name:", student2.school_name)
+
+print("How many students are there? ",Student.student_number)
+print("std1 ID:",student1.ID)
+print("std2 ID:",student2.ID)
+
+student1.I_method()
+Student.I_method(student1)
+
+
+"""
+Student.I_method()
+    does not work because  is an instance method and expects a parameter ().
+    When you call it directly from the class, Python does not automatically pass an instance, so you get a TypeError.
+    Instance methods belong to the object (instance). They require  to access instance-specific data.
+    Class methods and static methods belong to the class itself.
+    Class methods use  and can access class variables.
+    Static methods do not take  or  and behave like regular functions inside the class.
+
+"""
+
+print("------")
+
+class petshop():
+    pet_number = 0
+
+    def __init__(self,parent_name="-",pet_name="-"):
+        self.parent_name = parent_name
+        self.pet_name = pet_name
+        petshop.pet_number += 1
+
+    #instance method
+    def voice(self):
+        print(f"{self.pet_name} says: Woof!")
+    
+    #class method
+    @classmethod
+    def total_pets(cls):
+        print(f"Total pets in shop: {cls.pet_number}")
+
+    #static method
+    @staticmethod
+    def shop_info():
+        print("Welcome to the PetshopPy")
+
+pet1 = petshop("Hilal","Pamuk")
+pet2 = petshop("Ahmet","Cat")
+pet1.voice()
+pet1.shop_info()
+
+petshop.shop_info()
+petshop.total_pets()
---
+++ LearnLog Py/9-Inheritance.py
@@ -0,0 +1,80 @@
+class kitap():
+    kutuphaneninAdi = "Universite Kutuphanesi"
+    kitapSayisi = 0
+    kitapID = 1000
+
+    def __init__(self,ad="-",yazar="-",raf="-",sayfa="-",yayinevi="-"):
+        self.ad = ad
+        self.yazar =yazar
+        self.raf =raf
+        self.sayfa =sayfa
+        self.yayinevi =yayinevi
+        self.kitapSayisi += 1
+        self.kitapID += 10
+        kitap.kitapID = self.kitapID
+
+    #insatnce methods
+    def bilgileriGoster(self):
+        return print("Kitabin Adi: {}\n Kitabin Yazari: {}\n Sayfa: {}\n".format(self.ad,self.yazar,self.sayfa))
+    def sayfaArttir(self,deger):
+        self.sayfa += deger
+
+    #class methods
+    @classmethod
+    def toplamKitapSayisi(cls):
+        return kitap.kitapSayisi
+    @classmethod
+    def stringIleOlustur(cls,metin):
+        ad,yazar = metin.split("-")
+        return cls(ad,yazar)
+    
+    #static method
+    @staticmethod
+    def kutuphaneAdiSoyle():
+        return kitap.kutuphaneninAdi
+    
+
+#interitance
+class yerelKitaplar(kitap):
+    pass
+
+yerelKitap1 = yerelKitaplar("Calikusu","Resat Nuri")
+yerelKitap1.bilgileriGoster()
+
+#Overriding & super()
+class yabanciKitaplar(kitap):
+    def __init__(self,ad="-",yazar="-",raf="-",sayfa="-",yayinevi="-",cevirmen="-"):
+        super().__init__(ad,yazar,raf,sayfa,yayinevi)
+        self.cevirmen =cevirmen
+
+        
+    def bilgileriGoster(self):
+        return super().bilgileriGoster()
+
+#Dunder(magic,special) methods
+"""
+class Kitap:
+    def __init__(self, ad, yazar, sayfa):
+        self.ad = ad
+        self.yazar = yazar
+        self.sayfa = sayfa
+
+    def __str__(self):
+        return f"{self.ad} - {self.yazar} ({self.sayfa} sayfa)"
+
+    def __len__(self):
+        return self.sayfa
+
+    def __eq__(self, other):
+        return self.ad == other.ad and self.yazar == other.yazar
+
+# Test
+k1 = Kitap("Calikusu", "Resat Nuri", 200)
+k2 = Kitap("Calikusu", "Resat Nuri", 200)
+k3 = Kitap("Python 101", "Hilal", 150)
+
+print(k1)          # __str__ → Python 101 - Hilal (150 sayfa)
+print(len(k1))     # __len__ → 200
+print(k1 == k2)    # __eq__ → True
+print(k1 == k3)    # __eq__ → False
+"""
