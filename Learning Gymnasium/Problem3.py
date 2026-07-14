5f7ab573bb23c409a752e28f7c777817115a88be
Hilal Kepir <kepirhilal@gmail.com>
2026-07-14 10:01:44 +0300

    Update Problem3.py

--- Learning Gymnasium/Problem3.py
+++ Learning Gymnasium/Problem3.py
@@ -19,27 +19,36 @@ from gymnasium import spaces
 class depo_robotu(gym.Env):
     def __init__(self):
 
-        self.action_space = spaces.Discrete(3)
+        self.action_space = spaces.Discrete(4)
         
         self.observation_space = spaces.Dict(
             {
                 "robot_konumu":spaces.Discrete(3),
-                "paket_sirtta_mi":spaces.Discrete(2)
+                "paket_sirtta_mi":spaces.Discrete(2),
+                "pil_seviyesi":spaces.Discrete(4),
+                "engel_var_mi":spaces.Discrete(2),
+                "paket_tipi":spaces.Discrete(2)
+
             }
         )
 
         self.state = None
         self.sayac = 0
-        self.max_step = 15
+        self.max_step = 20
 
     def reset(self,seed = None, options = None):
-        
+        paket_tipi = np.random.choice([0,1]) # 0: hafif paket, 1: agir paket
+        engel_var_mi = np.random.choice([0,1]) # 0: engel yok, 1: engel var
+
         self.state = {
             "robot_konumu":0,
-            "paket_sirtta_mi":0
+            "paket_sirtta_mi":0,
+            "pil_seviyesi":self.max_pil,
+            "engel_var_mi":engel_var_mi,
+            "paket_tipi":paket_tipi            
         }
-        self.sayac = 0
 
+        self.sayac = 0
         return self.state,{}
     
 
